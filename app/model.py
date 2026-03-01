fb4ff9942bb1c9f6a92aeaf69d65957c4636188e
isaacMS300 <102270561+IsaacMS30@users.noreply.github.com>
2026-03-01 15:30:01 -0600

    Updated model filepath

--- app/model.py
+++ app/model.py
@@ -1,4 +1,9 @@
+from pathlib import Path
 import joblib
+
+# Get path
+BASE_DIR = Path(__file__).resolve().parent
+model_path = BASE_DIR / "models" / "model.joblib"
 
 # Load model
 model = joblib.load("app/models/model.joblib")
